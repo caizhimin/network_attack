ca8f488e04ba6392f7e4b7bae708492a5fc360d8
cai <jack_czm@vip.sina.com>
2016-01-12 13:14:34 +0800

    update model

--- flow/models.py
+++ flow/models.py
@@ -41,84 +41,84 @@ class IpAddressLib(models.Model):
     ProvinceLocation = models.CharField(max_length=30, null=True, blank=True, verbose_name='省份')
     CityLocation = models.CharField(max_length=30, null=True, blank=True, verbose_name='城市')
 
-class SQLInjecResult(models.Model):
-    Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
-    SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
-    SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
-    SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
-    DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
-    DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
-    DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
-    AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
-    SqlInjAttPackNum = models.IntegerField(blank=True, null=True, verbose_name='SQL注入攻击流量数据包个数')
-    SqlInjAttByte = models.IntegerField(blank=True, null=True, verbose_name='SQL注入攻击流量字节数')
-    SqlInjValidAtt = models.NullBooleanField(null=True, verbose_name='SQL注入有效攻击标识')
-    SqlInjAttType = models.IntegerField(blank=True, null=True, verbose_name='SQL注入攻击类型')
-    SqlInjAttLevel = models.IntegerField(blank=True, null=True, verbose_name='SQL注入攻击风险等级')
-
-    def __unicode__(self):
-        return self.DestIP
-
-
-class XSSResult(models.Model):
-    Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
-    SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
-    SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
-    SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
-    DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
-    DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
-    DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
-    AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
-    XssAttPackNum = models.IntegerField(blank=True, null=True, verbose_name='XSS攻击流量数据包个数')
-    XssAttByte = models.IntegerField(blank=True, null=True, verbose_name='Xss攻击流量字节数')
-    XssValidAtt = models.NullBooleanField(null=True, verbose_name='Xss有效攻击标识')
-    XssAttType = models.IntegerField(blank=True, null=True, verbose_name='Xss攻击类型')
-    XssAttLevel = models.IntegerField(blank=True, null=True, verbose_name='Xss攻击风险等级')
-
-
-class WebBDResult(models.Model):
-    Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
-    SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
-    SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
-    SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
-    DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
-    DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
-    DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
-    AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
-    WebShellAttPackNum = models.IntegerField(blank=True, null=True, verbose_name='Web后门攻击流量数据包个数')
-    WebShellAttByte = models.IntegerField(blank=True, null=True, verbose_name='Web后门攻击流量字节数')
-    WebShellValidAtt = models.NullBooleanField(null=True, verbose_name='Web后门有效攻击标识')
-    WebShellAttType = models.IntegerField(blank=True, null=True, verbose_name='Web后门攻击类型')
-    WebShellAttLevel = models.IntegerField(blank=True, null=True, verbose_name='Web后门攻击风险等级')
-
-
-class RCResult(models.Model):
-    Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
-    SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
-    SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
-    SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
-    DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
-    DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
-    DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
-    AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
-    RCE_AttPackNum = models.IntegerField(blank=True, null=True, verbose_name='远程命令执行攻击流量数据包个数')
-    RCE_AttByte = models.IntegerField(blank=True, null=True, verbose_name='远程命令执行攻击流量字节数')
-    RCE_ValidAtt = models.NullBooleanField(null=True, verbose_name='远程命令执行有效攻击标识')
-    RCE_AttType = models.IntegerField(blank=True, null=True, verbose_name='远程命令执行攻击类型')
-    RCE_AttLevel = models.IntegerField(blank=True, null=True, verbose_name='远程命令执行攻击风险等级')
-
-
-class FCResult(models.Model):
-    Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
-    SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
-    SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
-    SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
-    DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
-    DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
-    DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
-    AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
-    FileInAttPackNum = models.IntegerField(blank=True, null=True, verbose_name='文件包含攻击流量数据包个数')
-    FileInAttByte = models.IntegerField(blank=True, null=True, verbose_name='文件包含攻击流量字节数')
-    FileInValidAtt = models.NullBooleanField(null=True, verbose_name='文件包含有效攻击标识')
-    FileInAttType = models.IntegerField(blank=True, null=True, verbose_name='文件包含攻击类型')
-    FileInAttLevel = models.IntegerField(blank=True, null=True, verbose_name='文件包含攻击风险等级')
+# class SQLInjecResult(models.Model):
+#     Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
+#     SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
+#     SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
+#     SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
+#     DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
+#     DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
+#     DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
+#     AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
+#     SqlInjAttPackNum = models.IntegerField(blank=True, null=True, verbose_name='SQL注入攻击流量数据包个数')
+#     SqlInjAttByte = models.IntegerField(blank=True, null=True, verbose_name='SQL注入攻击流量字节数')
+#     SqlInjValidAtt = models.NullBooleanField(null=True, verbose_name='SQL注入有效攻击标识')
+#     SqlInjAttType = models.IntegerField(blank=True, null=True, verbose_name='SQL注入攻击类型')
+#     SqlInjAttLevel = models.IntegerField(blank=True, null=True, verbose_name='SQL注入攻击风险等级')
+#
+#     def __unicode__(self):
+#         return self.DestIP
+#
+#
+# class XSSResult(models.Model):
+#     Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
+#     SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
+#     SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
+#     SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
+#     DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
+#     DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
+#     DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
+#     AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
+#     XssAttPackNum = models.IntegerField(blank=True, null=True, verbose_name='XSS攻击流量数据包个数')
+#     XssAttByte = models.IntegerField(blank=True, null=True, verbose_name='Xss攻击流量字节数')
+#     XssValidAtt = models.NullBooleanField(null=True, verbose_name='Xss有效攻击标识')
+#     XssAttType = models.IntegerField(blank=True, null=True, verbose_name='Xss攻击类型')
+#     XssAttLevel = models.IntegerField(blank=True, null=True, verbose_name='Xss攻击风险等级')
+#
+#
+# class WebBDResult(models.Model):
+#     Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
+#     SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
+#     SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
+#     SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
+#     DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
+#     DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
+#     DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
+#     AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
+#     WebShellAttPackNum = models.IntegerField(blank=True, null=True, verbose_name='Web后门攻击流量数据包个数')
+#     WebShellAttByte = models.IntegerField(blank=True, null=True, verbose_name='Web后门攻击流量字节数')
+#     WebShellValidAtt = models.NullBooleanField(null=True, verbose_name='Web后门有效攻击标识')
+#     WebShellAttType = models.IntegerField(blank=True, null=True, verbose_name='Web后门攻击类型')
+#     WebShellAttLevel = models.IntegerField(blank=True, null=True, verbose_name='Web后门攻击风险等级')
+#
+#
+# class RCResult(models.Model):
+#     Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
+#     SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
+#     SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
+#     SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
+#     DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
+#     DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
+#     DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
+#     AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
+#     RCE_AttPackNum = models.IntegerField(blank=True, null=True, verbose_name='远程命令执行攻击流量数据包个数')
+#     RCE_AttByte = models.IntegerField(blank=True, null=True, verbose_name='远程命令执行攻击流量字节数')
+#     RCE_ValidAtt = models.NullBooleanField(null=True, verbose_name='远程命令执行有效攻击标识')
+#     RCE_AttType = models.IntegerField(blank=True, null=True, verbose_name='远程命令执行攻击类型')
+#     RCE_AttLevel = models.IntegerField(blank=True, null=True, verbose_name='远程命令执行攻击风险等级')
+#
+#
+# class FCResult(models.Model):
+#     Flow_ID = models.ForeignKey(Flow, null=True, blank=True, verbose_name='对应DPI流量ID')
+#     SrcIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='源IP')
+#     SrcPort = models.IntegerField(blank=True, null=True, verbose_name='源端口')
+#     SrcLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击来源地')
+#     DestIP = models.CharField(max_length=50, blank=True, null=True, verbose_name='目的IP')
+#     DescPort = models.IntegerField(blank=True, null=True, verbose_name='目的端口')
+#     DestLocation = models.CharField(max_length=100, blank=True, null=True, verbose_name='攻击目的地')
+#     AttTime = models.DateTimeField(blank=True, null=True, verbose_name='攻击时间')
+#     FileInAttPackNum = models.IntegerField(blank=True, null=True, verbose_name='文件包含攻击流量数据包个数')
+#     FileInAttByte = models.IntegerField(blank=True, null=True, verbose_name='文件包含攻击流量字节数')
+#     FileInValidAtt = models.NullBooleanField(null=True, verbose_name='文件包含有效攻击标识')
+#     FileInAttType = models.IntegerField(blank=True, null=True, verbose_name='文件包含攻击类型')
+#     FileInAttLevel = models.IntegerField(blank=True, null=True, verbose_name='文件包含攻击风险等级')
